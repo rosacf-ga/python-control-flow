ed09f9ef2ae68330d90aa2b624ff7aabe99b6b0d
rosacf-ga <rc.floress@yahoo.com>
2022-04-19 13:36:48 -0700

    Fix typo

--- exercise-2.py
+++ exercise-2.py
@@ -9,5 +9,5 @@
 
 phrase = input('Please enter a word or phrase: ')
 while phrase != 'quit':
-  print(f'What you entered is {len(phrase)} long')
+  print(f'What you entered is {len(phrase)} characters long')
   phrase = input('Please enter a word or phrase: ')
